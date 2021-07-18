10015cc85d8c5e831e6a64f7c428f3625d7a58ef
Francisco Puma <francis_pancho12@hotmail.com>
2021-07-18 09:03:21 -0700

    fixing routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,31 +41,79 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-  .then(dbTagData => {
-    if (!dbTagData) {
-      res.status(404).json({ message: "No category with a matching ID" });
-      return;
-    }
-    res.json(dbTagData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No category with a matching ID" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 
 
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No category with a matching ID" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No category with a matching ID" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
 module.exports = router;
